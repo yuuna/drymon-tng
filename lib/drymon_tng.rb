e652dc441a2effdaa6133eec5cb61f2f30149c07
Yuuna Kurita <yuuna.m@gmail.com>
2011-07-14 17:22:05 +0900

    require 忘れ

--- lib/drymon_tng.rb
+++ lib/drymon_tng.rb
@@ -4,6 +4,7 @@ require 'drymon_tng/pne/get'
 require 'drymon_tng/selenium'
 require 'drymon_tng/lime'
 require 'drymon_tng/config'
+require 'drymon_tng/pne/table'
 module DrymonTng
   VERSION = '0.0.0'
 end
